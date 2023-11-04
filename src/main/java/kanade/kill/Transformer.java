27af3abfecff071d4ce360761f337f1c4b19d192
root <herobrinetlb@gmail.com>
2023-11-04 13:27:36 +0800

    commit

--- src/main/java/kanade/kill/Transformer.java
+++ src/main/java/kanade/kill/Transformer.java
@@ -11,67 +11,72 @@ public class Transformer implements IClassTransformer {
     private Transformer(){}
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
-        if(transformedName.equals("net.minecraft.server.MinecraftServer")){
-            System.out.println("Get MinecraftServer.");
-            ClassReader cr = new ClassReader(basicClass);
-            ClassNode cn = new ClassNode();
-            cr.accept(cn,0);
-            for(MethodNode mn : cn.methods){
-                if (mn.name.equals("func_71190_q")) {
-                    InsnList list = new InsnList();
-                    list.add(new FieldInsnNode(Opcodes.GETSTATIC, "kanade/kill/Util", "killing", "Z"));
-                    LabelNode label = new LabelNode();
-                    list.add(new JumpInsnNode(Opcodes.IFEQ, label));
-                    list.add(new InsnNode(Opcodes.RETURN));
-                    list.add(label);
-                    list.add(new FrameNode(Opcodes.F_SAME, 0, null, 0, null));
-                    mn.instructions.insert(list);
-                    System.out.println("Insert return in updateTimeLightAndEntities.");
+        switch (transformedName) {
+            case "net.minecraft.server.MinecraftServer": {
+                System.out.println("Get MinecraftServer.");
+                ClassReader cr = new ClassReader(basicClass);
+                ClassNode cn = new ClassNode();
+                cr.accept(cn, 0);
+                for (MethodNode mn : cn.methods) {
+                    if (mn.name.equals("func_71190_q")) {
+                        InsnList list = new InsnList();
+                        list.add(new FieldInsnNode(Opcodes.GETSTATIC, "kanade/kill/Util", "killing", "Z"));
+                        LabelNode label = new LabelNode();
+                        list.add(new JumpInsnNode(Opcodes.IFEQ, label));
+                        list.add(new InsnNode(Opcodes.RETURN));
+                        list.add(label);
+                        list.add(new FrameNode(Opcodes.F_SAME, 0, null, 0, null));
+                        mn.instructions.insert(list);
+                        System.out.println("Insert return in updateTimeLightAndEntities.");
+                    }
                 }
+                ClassWriter cw = new ClassWriter(0);
+                cn.accept(cw);
+                return cw.toByteArray();
             }
-            ClassWriter cw = new ClassWriter(0);
-            cn.accept(cw);
-            return cw.toByteArray();
-        } else if (transformedName.equals("net.minecraft.client.Minecraft")) {
-            System.out.println("Get Minecraft.");
-            ClassReader cr = new ClassReader(basicClass);
-            ClassNode cn = new ClassNode();
-            cr.accept(cn,0);
-            for(MethodNode mn : cn.methods){
-                if (mn.name.equals("func_71407_l")) {
-                    InsnList list = new InsnList();
-                    list.add(new FieldInsnNode(Opcodes.GETSTATIC, "kanade/kill/Util", "killing", "Z"));
-                    LabelNode label = new LabelNode();
-                    list.add(new JumpInsnNode(Opcodes.IFEQ, label));
-                    list.add(new InsnNode(Opcodes.RETURN));
-                    list.add(label);
-                    list.add(new FrameNode(Opcodes.F_SAME, 0, null, 0, null));
-                    mn.instructions.insert(list);
-                    System.out.println("Insert return in runTick.");
+            case "net.minecraft.client.Minecraft": {
+                System.out.println("Get Minecraft.");
+                ClassReader cr = new ClassReader(basicClass);
+                ClassNode cn = new ClassNode();
+                cr.accept(cn, 0);
+                for (MethodNode mn : cn.methods) {
+                    if (mn.name.equals("func_71407_l")) {
+                        InsnList list = new InsnList();
+                        list.add(new FieldInsnNode(Opcodes.GETSTATIC, "kanade/kill/Util", "killing", "Z"));
+                        LabelNode label = new LabelNode();
+                        list.add(new JumpInsnNode(Opcodes.IFEQ, label));
+                        list.add(new InsnNode(Opcodes.RETURN));
+                        list.add(label);
+                        list.add(new FrameNode(Opcodes.F_SAME, 0, null, 0, null));
+                        mn.instructions.insert(list);
+                        System.out.println("Insert return in runTick.");
+                    }
                 }
+                ClassWriter cw = new ClassWriter(0);
+                cn.accept(cw);
+                return cw.toByteArray();
             }
-            ClassWriter cw = new ClassWriter(0);
-            cn.accept(cw);
-            return cw.toByteArray();
-        } else if (transformedName.equals("net.minecraft.item.ItemStack")) {
-            System.out.println("Get ItemStack.");
-            ClassReader cr = new ClassReader(basicClass);
-            ClassNode cn = new ClassNode();
-            cr.accept(cn, 0);
-            for (MethodNode mn : cn.methods) {
-                if (mn.name.equals("func_190926_b")) {
-                    InsnList list = new InsnList();
-                    LabelNode label = new LabelNode();
-                    list.add(new VarInsnNode(Opcodes.ALOAD, 0));
-                    list.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/item/ItemStack", "item", "Lnet/minecraft/item/Item;"));
-                    list.add(new FieldInsnNode(Opcodes.GETSTATIC, "kanade/kill/ModMain", "kill_item", "Lnet/minecraft/item/Item;"));
-                    list.add(new JumpInsnNode(Opcodes.IF_ACMPNE, label));
-                    list.add(new InsnNode(Opcodes.ICONST_0));
-                    list.add(new InsnNode(Opcodes.IRETURN));
-                    list.add(label);
-                    list.add(new FrameNode(Opcodes.F_SAME, 0, null, 0, null));
-                    System.out.println("Insert return in isEmpty.");
+            case "net.minecraft.item.ItemStack": {
+                System.out.println("Get ItemStack.");
+                ClassReader cr = new ClassReader(basicClass);
+                ClassNode cn = new ClassNode();
+                cr.accept(cn, 0);
+                for (MethodNode mn : cn.methods) {
+                    if (mn.name.equals("func_190926_b")) {
+                        InsnList list = new InsnList();
+                        LabelNode label = new LabelNode();
+                        list.add(new VarInsnNode(Opcodes.ALOAD, 0));
+                        list.add(new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/item/ItemStack", "item", "Lnet/minecraft/item/Item;"));
+                        list.add(new FieldInsnNode(Opcodes.GETSTATIC, "kanade/kill/ModMain", "kill_item", "Lnet/minecraft/item/Item;"));
+                        list.add(new JumpInsnNode(Opcodes.IF_ACMPNE, label));
+                        list.add(new InsnNode(Opcodes.ICONST_0));
+                        list.add(new InsnNode(Opcodes.IRETURN));
+                        list.add(label);
+                        list.add(new FrameNode(Opcodes.F_SAME, 0, null, 0, null));
+                        System.out.println("Insert return in isEmpty.");
+                    }
                 }
+                break;
             }
         }
         return basicClass;
